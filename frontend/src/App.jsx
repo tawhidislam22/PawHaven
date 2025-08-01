771303c5000103dde6d1d6700face37948225738
tawhidislam22 <tawhidislam144@gmail.com>
2025-08-02 02:12:12 +0600

    added: daisy ui added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,10 +8,75 @@ function App() {
 
   return (
     <>
+      <div className="carousel w-full">
+        <div id="item1" className="carousel-item w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+            className="w-full" />
+        </div>
+        <div id="item2" className="carousel-item w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+            className="w-full" />
+        </div>
+        <div id="item3" className="carousel-item w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+            className="w-full" />
+        </div>
+        <div id="item4" className="carousel-item w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+            className="w-full" />
+        </div>
+      </div>
+      <div className="flex w-full justify-center gap-2 py-2">
+        <a href="#item1" className="btn btn-xs">1</a>
+        <a href="#item2" className="btn btn-xs">2</a>
+        <a href="#item3" className="btn btn-xs">3</a>
+        <a href="#item4" className="btn btn-xs">4</a>
+      </div>
       <div>
         <h1 className='text-3xl bg-red-500 font-bold underline'>Hello World</h1>
       </div>
-      
+      <div className="card w-96 bg-base-100 shadow-sm">
+        <div className="card-body">
+          <span className="badge badge-xs badge-warning">Most Popular</span>
+          <div className="flex justify-between">
+            <h2 className="text-3xl font-bold">Premium</h2>
+            <span className="text-xl">$29/mo</span>
+          </div>
+          <ul className="mt-6 flex flex-col gap-2 text-xs">
+            <li>
+              <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+              <span>High-resolution image generation</span>
+            </li>
+            <li>
+              <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+              <span>Customizable style templates</span>
+            </li>
+            <li>
+              <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+              <span>Batch processing capabilities</span>
+            </li>
+            <li>
+              <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+              <span>AI-driven image enhancements</span>
+            </li>
+            <li className="opacity-50">
+              <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+              <span className="line-through">Seamless cloud integration</span>
+            </li>
+            <li className="opacity-50">
+              <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+              <span className="line-through">Real-time collaboration tools</span>
+            </li>
+          </ul>
+          <div className="mt-6">
+            <button className="btn btn-primary btn-block">Subscribe</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
